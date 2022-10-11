1bbaec628f93bb0bfbd5d0352ea12f900cce1f9f
Hazar Nazari <hazar.nazari@kuehne-nagel.com>
2022-10-11 14:04:15 +0200

    something

--- src/main/kotlin/example/collections/CollectionPractice.kt
+++ src/main/kotlin/example/collections/CollectionPractice.kt
@@ -1,25 +1,37 @@
 package example.collections
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.stream.IntStream
+
 fun main(){
-    val languages = listOf("Java", "Kotlin", "Javascript")
-//
-//    languages.forEach {
-//        println(it)
+
+    val currentTime = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm"))
+    println(currentTime)
+//    val names = listOf("first", "second")
+//    if("second" in names){
+//        println("Yes, it exist")
 //    }
+
+//    IntStream.rangeClosed(1, 10).map{ Person("name$it", age = 2, dateOfBirth = Date()) }
+//        .map{ it -> Person(it.) }
+//
 //
 //    val languageSet = setOf("java", "kotlin", "java")
 //    languageSet.forEach{println(it)}
-//
+////
 //    val studentCourse = mapOf("Chemistry" to 4, "Math" to 100)
 //    studentCourse.forEach{
 //        println("Course Name: " + it.key + ", student number: " + it.value)
 //    }
-
-    languages.transferToUpperCase()
-    languages.forEach{println(it)}
+//
+//    languages.transferToUpperCase()
+//    languages.forEach{println(it)}
 }
 
 fun List<String>.transferToUpperCase(){
     this.map { it.toUpperCase() }
 }
 
+data class Person(val name: String, val age: Int, val dateOfBirth: Date)
